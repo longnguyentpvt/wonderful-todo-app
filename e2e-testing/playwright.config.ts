22fd7f5b38bf02d218bc377ab68af08c2ff50d8a
Long <>
2024-06-17 08:55:59 +0700

    feat: add test cases to check UI rendering

--- e2e-testing/playwright.config.ts
+++ e2e-testing/playwright.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  timeout: 30000,
+  expect: {
+    timeout: 5000
+  },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
--- e2e-testing/tests/prod.spec.ts
+++ e2e-testing/tests/prod.spec.ts
@@ -1,7 +1,53 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+
+let page: Page;
+
+test.describe.configure({ mode: 'serial' });
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test('open Prod', async () => {
   await page.goto('http://15.235.146.167:3001/todo-app/tasks');
+});
 
+test('has title', async () => {
   await expect(page).toHaveTitle("Wonderful Todo App");
+});
+
+test('has breadcrumb', async () => {
+  const breadcrumbSelector = 'div.fs-3';
+  const breadcrumbLocator = page.locator(breadcrumbSelector);
+  await page.waitForSelector(breadcrumbSelector);
+
+  const isVisible = await breadcrumbLocator.isVisible();
+  expect(isVisible).toBeTruthy();
+
+  const text = await breadcrumbLocator.innerText();
+  expect(text).toBe("Todo List");
+});
+
+test('has new task button', async () => {
+  const buttonLocator = page.getByRole('button', { name: /\+ NEW TASK/i });
+
+  const isVisible = await buttonLocator.isVisible();
+  expect(isVisible).toBeTruthy();
+});
+
+test('able to open Create modal', async () => {
+  const buttonLocator = page.getByRole('button', { name: /\+ NEW TASK/i });
+
+  await buttonLocator.click();
+
+  const modalLocator = page.locator('div.offcanvas');
+  const title = modalLocator.locator('.offcanvas-title');
+
+  const titleText = await title.innerText();
+  expect(titleText).toBe('Detail');
 });
